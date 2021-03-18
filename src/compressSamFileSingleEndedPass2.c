63515f2d9d3ba154c0fc26ba154590311a423308
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-03-18 17:39:56 -0500

    Update compressSamFileSingleEndedPass2.c

--- src/compressSamFileSingleEndedPass2.c
+++ src/compressSamFileSingleEndedPass2.c
@@ -206,12 +206,13 @@ void compressSimilarAlignments(char *input_filename, char *output_abridgefilenam
 		{
 			splitByDelimiter(split_on_tab[1], ',', split_icigars);
 			number_of_cigars = splitByDelimiter(split_on_tab[2], ',', split_num_reads);
-
+			/*
 			if (number_of_cigars>=10)
 			  for(i=number_of_cigars-10;i<number_of_cigars;i++)
 				printf("\n%d %s %s",number_of_cigars, split_icigars[i],split_num_reads[i]);
 			if (number_of_cigars>=10)
 			  printf("\n%d ====================================================================================================================",number_of_cigars);
+			*/
 			splitByDelimiter(split_on_tab[1], ',', split_icigars_cp);
 			splitByDelimiter(split_on_tab[2], ',', split_num_reads_cp);
 			if (line_num == -1)
