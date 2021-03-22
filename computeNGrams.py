155ff664fea5bd6928d1705745be9fd6ab87f30d
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-03-22 06:14:33 -0500

    Update computeNGrams.py

--- computeNGrams.py
+++ computeNGrams.py
@@ -1,11 +1,13 @@
+from collections import Counter 
 import pprint
 
 def find_ngrams(input_list, n):
-  return list(zip(*[input_list[i:] for i in range(n)]))
+    return Counter(input_list[idx : idx + n] for idx in range(len(input_list) - 1)) 
+  
 
 
 input_filename = "/90daydata/maizegdb/sagnik/ABRIDGE/developing_abridge/SRR13711355_0_SE_Aligned.sortedByCoord.out.sam.qual.rle"
 fhr=open(input_filename,"r")
 for line in fhr:
-    pprint.pprint(find_ngrams(list(line.strip()), 3))
+    pprint.pprint(str(dict(find_ngrams(list(line.strip()), 3))))
 fhr.close()
