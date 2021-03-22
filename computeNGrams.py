390b4ab082645fbb4f60a9db68dd02ddab8f94b7
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-03-22 06:18:34 -0500

    Update computeNGrams.py

--- computeNGrams.py
+++ computeNGrams.py
@@ -6,10 +6,16 @@ def find_ngrams(input_list, n):
   
 
 
-input_filename = "/90daydata/maizegdb/sagnik/ABRIDGE/developing_abridge/SRR13711355_0_SE_Aligned.sortedByCoord.out.sam.qual.rle"
+input_filename = "/90daydata/maizegdb/sagnik/ABRIDGE/developing_abridge/SRR13711355_0_SE_Aligned.sortedByCoord.out.sam.qual.rle.line1"
 fhr=open(input_filename,"r")
 for line_number,line in enumerate(fhr):
-    print(line_number+1)
+    print(f"Unigram {line_number+1}")
+    print(str(dict(find_ngrams(line.strip(), 1))))
+    print("="*100)
+    print(f"Bigram {line_number+1}")
+    print(str(dict(find_ngrams(line.strip(), 2))))
+    print("="*100)
+    print(f"Trigram {line_number+1}")
     print(str(dict(find_ngrams(line.strip(), 3))))
     print("="*100)
 fhr.close()
