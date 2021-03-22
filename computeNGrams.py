c80ff9f1e478927052a374d16233d593448dab91
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-03-22 06:06:40 -0500

    Create computeNGrams.py

---
+++ computeNGrams.py
@@ -0,0 +1,11 @@
+import pprint
+
+def find_ngrams(input_list, n):
+  return zip(*[input_list[i:] for i in range(n)])
+
+
+input_filename = "/90daydata/maizegdb/sagnik/ABRIDGE/developing_abridge/SRR13711355_0_SE_Aligned.sortedByCoord.out.sam.qual.rle"
+fhr=open(input_filename,"r")
+for line in fhr:
+    pprint.pprint(find_ngrams(list(line.strip()), 3))
+fhr.close()
