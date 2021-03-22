533ff9ca74eca55a56c30ddc8807d08fb9e8f296
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-03-22 06:39:21 -0500

    Update computeNGrams.py

--- computeNGrams.py
+++ computeNGrams.py
@@ -6,12 +6,22 @@ def find_ngrams(input_list, n):
     return sorted(res.items(),key=lambda item: (-item[1], item[0])) 
 
 input_filename = "/90daydata/maizegdb/sagnik/ABRIDGE/developing_abridge/SRR13711355_0_SE_Aligned.sortedByCoord.out.sam.qual.rle.line3"
+output_filename = "/90daydata/maizegdb/sagnik/ABRIDGE/developing_abridge/SRR13711355_0_SE_Aligned.sortedByCoord.out.sam.qual.rle.line3.trigramcompression"
 fhr=open(input_filename,"r")
 for line_number,line in enumerate(fhr):
     bigram = dict(find_ngrams(line.strip(), 2))
     trigram = dict(find_ngrams(line.strip(), 3))
-    for key,value in trigram.iteritems():
-        print(key,value)
+    conversion_table = {}
+    starting_ASCII_code_for_single_character_replacement = 74
+    for key in trigram:
+        print(key,trigram[key])
+        if trigram[key]>1000:
+            line = line.replace(key,chr(starting_ASCII_code_for_single_character_replacement))
+            conversion_table[chr(starting_ASCII_code_for_single_character_replacement)]=key
+            starting_ASCII_code_for_single_character_replacement+=1
+    fhw = open(output_filename,"w")
+    fhw.write(line)
+    fhw.close()
     """
     print(f"Unigram {line_number+1}")
     print(str(dict(find_ngrams(line.strip(), 1))))
