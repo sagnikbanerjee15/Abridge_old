f869e304bc66e22e284a1497ef0acf173d95cae7
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-03-22 06:15:30 -0500

    Update computeNGrams.py

--- computeNGrams.py
+++ computeNGrams.py
@@ -9,5 +9,5 @@ def find_ngrams(input_list, n):
 input_filename = "/90daydata/maizegdb/sagnik/ABRIDGE/developing_abridge/SRR13711355_0_SE_Aligned.sortedByCoord.out.sam.qual.rle"
 fhr=open(input_filename,"r")
 for line in fhr:
-    pprint.pprint(str(dict(find_ngrams(line.strip()), 3)))
+    pprint.pprint(str(dict(find_ngrams(line.strip(), 3))))
 fhr.close()
