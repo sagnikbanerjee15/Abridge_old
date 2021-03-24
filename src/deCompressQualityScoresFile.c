950209bd6a9244bbe30cd49d605626c88ee5a91e
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-03-24 14:39:12 -0500

    Update deCompressQualityScoresFile.c

--- src/deCompressQualityScoresFile.c
+++ src/deCompressQualityScoresFile.c
@@ -104,7 +104,7 @@ void convertRLEtoQualValues ( char *input_qualityscore_filename, char *output_qu
 	}
 	max_read_length = rle_quality_scores_index;
 	quality_score_of_read = ( char* ) malloc ( sizeof(char) * max_read_length );
-
+	return;
 	/*
 	 * Calculate sum of each position
 	 */
