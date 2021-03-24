9a775214749f6fd1e30142fd228280fb6091f320
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-03-24 14:46:44 -0500

    Update deCompressQualityScoresFile.c

--- src/deCompressQualityScoresFile.c
+++ src/deCompressQualityScoresFile.c
@@ -69,6 +69,8 @@ void convertRLEtoQualValues ( char *input_qualityscore_filename, char *output_qu
 			if ( isdigit ( line[i] ) == 0 )
 				number_of_quality_scores_in_current_position++;
 		}
+		printf ( "\n%d %d" , rle_quality_scores_index , number_of_quality_scores_in_current_position );
+		fflush ( stdout );
 		rle_quality_scores[rle_quality_scores_index] = ( struct RLE_Quality_Scores* ) malloc ( sizeof(struct RLE_Quality_Scores) * number_of_quality_scores_in_current_position );
 		//number_of_quality_scores_in_current_position_index = 0;
 		for ( i = 0 ; line[i] != '\0' ; i++ )
