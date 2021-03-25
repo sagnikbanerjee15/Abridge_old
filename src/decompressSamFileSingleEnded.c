0cf7abe1c16885d021f25752e83f887fd1ceed87
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-03-25 12:17:55 -0500

    Update decompressSamFileSingleEnded.c

--- src/decompressSamFileSingleEnded.c
+++ src/decompressSamFileSingleEnded.c
@@ -152,8 +152,8 @@ void decompressFile (char *name_of_file_with_quality_scores, char *abridge_index
 				free (split_on_newline[j]);
 			free (split_on_newline);
 			split_on_newline = ( char** ) malloc (sizeof(char*) * number_of_newlines);
-			for ( i = 0 ; i < number_of_newlines ; i++ )
-				split_on_newline[i] = ( char* ) malloc (sizeof(char) * COLS * 10);
+			for ( j = 0 ; j < number_of_newlines ; j++ )
+				split_on_newline[j] = ( char* ) malloc (sizeof(char) * COLS * 10);
 		}
 		printf ("\nLine num %d Number of newlines %d" , i , number_of_newlines);
 		fflush (stdout);
