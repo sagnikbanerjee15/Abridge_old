6c16dff1c4d7e52a49d6701f683d5a15415fcacc
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-03-27 16:35:29 -0500

    Update decompressSamFileSingleEnded.c

--- src/decompressSamFileSingleEnded.c
+++ src/decompressSamFileSingleEnded.c
@@ -176,6 +176,7 @@ void decompressFile (char *name_of_file_with_quality_scores, char *abridge_index
 		if ( line_len > COLS_split_on_tab || max_number_of_commas > ROWS_split_on_comma )
 		{
 			printf ("\n line_len %d COLS_split_on_tab %d max_number_of_commas %d ROWS_split_on_comma %d" , line_len , COLS_split_on_tab , max_number_of_commas , ROWS_split_on_comma);
+			fflush (stdout);
 			for ( i = 0 ; i < COLS_split_on_tab ; i++ )
 				free (split_on_tab[i]);
 			COLS_split_on_tab = line_len + 100;
