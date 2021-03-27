34455f85eecb87396c73a732b07f66ba8c57597f
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-03-27 16:21:38 -0500

    Update decompressSamFileSingleEnded.c

--- src/decompressSamFileSingleEnded.c
+++ src/decompressSamFileSingleEnded.c
@@ -165,10 +165,10 @@ void decompressFile (char *name_of_file_with_quality_scores, char *abridge_index
 				line_len = getline ( &buffer , &len , fhr);
 			} while ( buffer[0] == '@' );
 
-			number_of_columns = splitByDelimiter (buffer , '\t' , split_on_tab);
-			curr_position = strtol (split_on_tab[0] , &convert_to_int_temp , 10);
-			curr_position--;
-			printf ("\n%s %d %d %s" , current_chromosome , curr_position , number_of_columns , buffer);
+			//number_of_columns = splitByDelimiter (buffer , '\t' , split_on_tab);
+			//curr_position = strtol (split_on_tab[0] , &convert_to_int_temp , 10);
+			curr_position = 0;
+			//printf ("\n%s %d %d %s" , current_chromosome , curr_position , number_of_columns , buffer);
 		}
 		number_of_commas_in_each_line = 0;
 		for ( i = 0 ; buffer[i] != '\0' ; i++ )
