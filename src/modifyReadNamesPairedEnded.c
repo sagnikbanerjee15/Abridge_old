61769f514e4aed3e021dd22ee0bf1d6a7ab937d4
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-04-05 16:29:49 -0500

    Create modifyReadNamesPairedEnded.c

---
+++ src/modifyReadNamesPairedEnded.c
@@ -0,0 +1,92 @@
+# include <stdio.h>
+# include <stdint.h>
+# include <string.h>
+# include <stdlib.h>
+# include <ctype.h>
+# include <math.h>
+# include <stdbool.h>
+# include "data_structure_definitions.h"
+# include "function_definitions.h"
+
+void generateNextReadID (int *alphabets, char *read_id, int *read_length)
+{
+	/********************************************************************
+	 * Variable declaration
+	 ********************************************************************/
+	int i;
+
+	int check_for_last_read_id[100];
+	/********************************************************************/
+
+	/********************************************************************
+	 * Variable initialization
+	 ********************************************************************/
+
+	/********************************************************************/
+
+	if ( *read_length == 0 )
+	{
+		read_id[0] = 0;
+		( *read_length )++;
+	}
+	else
+	{
+		/*
+		 * Check if the read is the last element of the maximum read_length
+		 */
+		for ( i = 0 ; i < *read_length ; i++ )
+			if ( read_id[i] != strlen (alphabets) - 1 ) break;
+		if ( i != *read_length )
+		{
+			( *read_length )++;
+			for ( i = 0 ; i < *read_length ; i++ )
+				read_id[i] = 0;
+		}
+		else
+		{
+			/*
+			 * Increment the read_id
+			 */
+			for ( i = *read_length - 1 ; i >= 0 ; i-- )
+			{
+				if ( read_id[i] == ( strlen (alphabets) - 1 ) )
+					read_id[i] = 0;
+				else
+				{
+					read_id[i]++;
+					break;
+				}
+			}
+		}
+	}
+}
+
+int main (int argc, char *argv[])
+{
+	/********************************************************************
+	 * Variable declaration
+	 ********************************************************************/
+	int read_length;
+	int read_id[100];
+	int i, j;
+
+	char alphabets[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	/********************************************************************/
+
+	/********************************************************************
+	 * Variable initialization
+	 ********************************************************************/
+	read_length = 0;
+
+	/********************************************************************/
+	for ( i = 0 ; i < 1000 ; i++ )
+	{
+		generateNextReadID (alphabets , read_id , &read_length);
+		for ( j = 0 ; j < read_length ; j++ )
+			printf ("%c" , alphabets[read_id[i]]);
+		printf ("\n");
+	}
+
+	return 0;
+}
