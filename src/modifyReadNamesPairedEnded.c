b9588b2a243d2b5e796045237df69d1a7ebb23d1
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-04-10 08:29:15 -0500

    Update modifyReadNamesPairedEnded.c

--- src/modifyReadNamesPairedEnded.c
+++ src/modifyReadNamesPairedEnded.c
@@ -393,11 +393,11 @@ void convertOldReadIdsToNewReadIds (char *input_samfilename, char *output_samfil
 		else
 		{
 			generateNextReadID (alphabets , read_id , &read_length);
-
-			printf ("\nread_length=%d Read Id indices " , read_length);
-			for ( i = 0 ; i < read_length ; i++ )
-				printf ("%d " , read_id[i]);
-
+			/*
+			 printf ("\nread_length=%d Read Id indices " , read_length);
+			 for ( i = 0 ; i < read_length ; i++ )
+			 printf ("%d " , read_id[i]);
+			 */
 			convertReadIdToString (read_id , read_id_string , read_length , alphabets);
 			//printf ("\nNew Read id %s" , read_id_string);
 			//fflush (stdout);
