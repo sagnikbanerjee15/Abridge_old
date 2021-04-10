1c7d7422c2aa4f51e9bd7f11b283547faa646d1a
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-04-10 07:56:11 -0500

    Update modifyReadNamesPairedEnded.c

--- src/modifyReadNamesPairedEnded.c
+++ src/modifyReadNamesPairedEnded.c
@@ -186,6 +186,7 @@ void insertNodeInCircularLinkedList (char *old_read_id, char *new_read_id, int N
 		printf ("\nCreating the first node");
 		fflush (stdout);
 		head = allocateMemoryOld_Read_ID_to_New_Read_ID_Circular_Linked_list ();
+		return;
 		head->prev = head;
 		strcpy(head->new_read_id , new_read_id);
 		strcpy(head->old_read_id , old_read_id);
