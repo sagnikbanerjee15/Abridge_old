2599835f8dde69fcf72ce678d4ea6a3c1562c3d8
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-04-10 07:13:56 -0500

    Update modifyReadNamesPairedEnded.c

--- src/modifyReadNamesPairedEnded.c
+++ src/modifyReadNamesPairedEnded.c
@@ -401,6 +401,7 @@ void convertOldReadIdsToNewReadIds (char *input_samfilename, char *output_samfil
 			printf ("\ntotal_number_of_nodes_created %d" , total_number_of_nodes_created);
 			fflush (stdout);
 			insertNodeInCircularLinkedList ( &head , split_line[0] , read_id_string , NH_value , &total_number_of_nodes_created , &number_of_invalid_nodes);
+			exit (1);
 			continue;
 			strcpy(split_line[0] , read_id_string);
 			//printf ("\nRight after inserting node %d" , head == NULL);
