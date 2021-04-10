e43511342778412144799e1423f8ff685a9feb1a
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-04-09 19:31:52 -0500

    Update modifyReadNamesPairedEnded.c

--- src/modifyReadNamesPairedEnded.c
+++ src/modifyReadNamesPairedEnded.c
@@ -13,7 +13,7 @@ void writeToFile (char **split_line, FILE *fhw)
 	fprintf (fhw , "%s" , split_line[0]);
 	fprintf (fhw , "%s" , "\t");
 	fprintf (fhw , "%s" , split_line[1]);
-	fflush (fhw);
+	//fflush (fhw);
 }
 
 void generateNextReadID (char *alphabets, int *read_id, int *read_length)
