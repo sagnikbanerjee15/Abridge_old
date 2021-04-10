58be7be116ee10476d1c20c55b0eebefe2bb2987
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-04-10 07:26:06 -0500

    Update modifyReadNamesPairedEnded.c

--- src/modifyReadNamesPairedEnded.c
+++ src/modifyReadNamesPairedEnded.c
@@ -199,6 +199,7 @@ void insertNodeInCircularLinkedList (struct Old_Read_ID_to_New_Read_ID_Circular_
 		fflush (stdout);
 		struct Old_Read_ID_to_New_Read_ID_Circular_Linked_list *temp = NULL;
 		struct Old_Read_ID_to_New_Read_ID_Circular_Linked_list *node = NULL;
+		return;
 		temp = head;
 		return;
 		if ( temp->prev == head ) //Only one node in linked list
