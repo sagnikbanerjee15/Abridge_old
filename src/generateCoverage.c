5477249d5acd3c6eb4f9a3c7b6ce8489f8992e1f
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-05-12 18:06:27 -0500

    Update generateCoverage.c

--- src/generateCoverage.c
+++ src/generateCoverage.c
@@ -247,9 +247,10 @@ void generateCoverageFromCompressedMappedFile (char *pass1_filename, char *abrid
 		if ( split == 0 )
 		{
 			if ( d == 1 && bg == 0 && bga == 0 )
-			{
-				for ( j = 0 ; j < length_of_continuous_segment ; j++ )
-					printf ("%s\t%d\t%d\n" , abridge_index->chromosome[i] , abridge_index->start[i] + j , coverage_array[j]);
+			{/*
+			 for ( j = 0 ; j < length_of_continuous_segment ; j++ )
+			 printf ("\n%s\t%d\t%d" , abridge_index->chromosome[i] , abridge_index->start[i] + j , coverage_array[j]);
+			 */
 			}
 			else if ( d == 0 && bg == 1 && bga == 0 )
 			{
