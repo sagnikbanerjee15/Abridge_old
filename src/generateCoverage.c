06e01698a2133fe42796c93102511d1ca04410e3
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-05-12 17:00:04 -0500

    Update generateCoverage.c

--- src/generateCoverage.c
+++ src/generateCoverage.c
@@ -147,6 +147,7 @@ void generateCoverageFromCompressedMappedFile (char *pass1_filename, char *abrid
 		while ( number_of_bytes_read_from_compressed_file < max_bytes_for_current_index_entry )
 		{
 			printf ("number_of_bytes_read_from_compressed_file = %d max_bytes_for_current_index_entry = %d" , number_of_bytes_read_from_compressed_file , max_bytes_for_current_index_entry);
+			fflush (stdout);
 			line_len = getline ( &buffer_for_pass1 , &len , fhr_pass1);
 			if ( buffer_for_pass1[0] == '@' ) continue;
 			number_of_bytes_read_from_compressed_file += line_len;
