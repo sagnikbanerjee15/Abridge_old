e8bf4ee6947233b18b999f55d79979be8443318c
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-05-14 11:11:55 -0500

    Update generateCoverage.c

--- src/generateCoverage.c
+++ src/generateCoverage.c
@@ -398,7 +398,7 @@ void generateCoverageFromCompressedMappedFile (char *pass1_filename, char *abrid
 				printf ("\n%s\t%d\t%d\t%d Last record" , abridge_index->chromosome[i] , abridge_index->start[i] + local_start - 1 , abridge_index->start[i] + local_end , previous_coverage);
 			prev_stopping_location = abridge_index->start[i] + j - 1;
 		}
-		printf (" Cluster end Previous stopping location %d " , prev_stopping_location);
+		printf (" Cluster end Previous stopping location %d cluster start %d length of continuous segment %d" , prev_stopping_location , abridge_index->start[i] , length_of_continuous_segment);
 	}
 
 	/*
