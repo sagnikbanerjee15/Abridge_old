3d4a7ab55fd98083a894dfec0da895c6cb7955b7
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-05-14 08:58:42 -0500

    Update generateCoverage.c

--- src/generateCoverage.c
+++ src/generateCoverage.c
@@ -360,7 +360,7 @@ void generateCoverageFromCompressedMappedFile (char *pass1_filename, char *abrid
 			}
 			else if ( first_position == 0 )
 			{
-				printf ("\n%s\t%d\t%d\t%d" , abridge_index->chromosome[i] , prev_stopping_location + 1 - 1 , abridge_index->start[i] , 0);
+				printf ("\n%s\t%d\t%d\t%dThis Location" , abridge_index->chromosome[i] , prev_stopping_location + 1 - 1 , abridge_index->start[i] , 0);
 			}
 			int local_start, local_end, previous_coverage;
 			int print_last_record = 0;
