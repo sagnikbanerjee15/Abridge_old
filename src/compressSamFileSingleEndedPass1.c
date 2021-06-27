963a1b848716b45b5a597e7c77f54da4bed90895
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-06-27 12:48:29 -0500

    Update compressSamFileSingleEndedPass1.c

--- src/compressSamFileSingleEndedPass1.c
+++ src/compressSamFileSingleEndedPass1.c
@@ -98,7 +98,10 @@ void writeToFile (short int flag_save_all_quality_scores, FILE *fhw_qual, FILE *
 					case 'U':
 						for ( k = strlen (compressed_ds_pool[i]->pointers_to_qual_scores[j]) - 1 ;
 								k >= 0 ; k-- )
+						{
 							qual[strlen (compressed_ds_pool[i]->pointers_to_qual_scores[j]) - 1 - k] = compressed_ds_pool[i]->pointers_to_qual_scores[j][k] - 90;
+							printf ("\n Read namne check %s" , compressed_ds_pool[i]->pointers_to_read_names[j]);
+						}
 						qual[strlen (compressed_ds_pool[i]->pointers_to_qual_scores[j])] = '\0';
 						break;
 				}
