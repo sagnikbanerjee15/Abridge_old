1af5345aaa662daf944e0ce2c34c6ae60452a185
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-07-03 18:16:58 -0500

    push

--- src/deCompressQualityScoresFile.c
+++ src/deCompressQualityScoresFile.c
@@ -6,7 +6,7 @@
 # include "data_structure_definitions.h"
 # include "function_definitions.h"
 
-void convertRLEtoQualValues ( char *input_qualityscore_filename, char *output_quality_score_filename )
+void convertRLEtoQualValues (char *input_qualityscore_filename, char *output_quality_score_filename)
 {
 	/********************************************************************
 	 * Variable declaration
@@ -17,7 +17,7 @@ void convertRLEtoQualValues ( char *input_qualityscore_filename, char *output_qu
 	size_t len = 0;
 	ssize_t line_len;
 
-	int i , j , k;
+	int i, j, k;
 	int num;
 	int max_read_length;
 	int rle_quality_scores_index = 0;
@@ -37,22 +37,22 @@ void convertRLEtoQualValues ( char *input_qualityscore_filename, char *output_qu
 	/********************************************************************
 	 * Variable initialization
 	 ********************************************************************/
-	fhr = fopen ( input_qualityscore_filename , "r" );
+	fhr = fopen (input_qualityscore_filename , "r");
 	if ( fhr == NULL )
 	{
-		printf ( "Error! File %s not found" , input_qualityscore_filename );
-		exit ( 1 );
+		printf ("Error! File %s not found" , input_qualityscore_filename);
+		exit (1);
 	}
-	fhw = fopen ( output_quality_score_filename , "w" );
+	fhw = fopen (output_quality_score_filename , "w");
 	if ( fhw == NULL )
 	{
-		printf ( "%s File cannot be created" , output_quality_score_filename );
-		exit ( 1 );
+		printf ("%s File cannot be created" , output_quality_score_filename);
+		exit (1);
 	}
 
-	rle_quality_scores = ( struct RLE_Quality_Scores** ) malloc ( sizeof(struct RLE_Quality_Scores*) * MAX_SEQ_LEN );
-	quality_score_position_max = ( int* ) malloc ( sizeof(int) * MAX_SEQ_LEN );
-	quality_score_position_index = ( int* ) malloc ( sizeof(int) * MAX_SEQ_LEN );
+	rle_quality_scores = ( struct RLE_Quality_Scores** ) malloc (sizeof(struct RLE_Quality_Scores*) * MAX_SEQ_LEN);
+	quality_score_position_max = ( int* ) malloc (sizeof(int) * MAX_SEQ_LEN);
+	quality_score_position_index = ( int* ) malloc (sizeof(int) * MAX_SEQ_LEN);
 	for ( i = 0 ; i < MAX_SEQ_LEN ; i++ )
 	{
 		quality_score_position_index[i] = 0;
@@ -61,19 +61,19 @@ void convertRLEtoQualValues ( char *input_qualityscore_filename, char *output_qu
 	/********************************************************************/
 
 	max_read_length = 0;
-	while ( ( line_len = getline ( &line , &len , fhr ) ) != -1 )
+	while ( ( line_len = getline ( &line , &len , fhr) ) != -1 )
 	{
 		number_of_quality_scores_in_current_position = 0;
 		for ( i = 0 ; line[i] != '\0' ; i++ )
 		{
-			if ( isdigit ( line[i] ) == 0 )
+			if ( isdigit (line[i]) == 0 )
 				number_of_quality_scores_in_current_position++;
 		}
-		/*
-		 printf ( "\n%d %d" , rle_quality_scores_index , number_of_quality_scores_in_current_position );
-		 fflush ( stdout );
-		 */
-		rle_quality_scores[rle_quality_scores_index] = ( struct RLE_Quality_Scores* ) malloc ( sizeof(struct RLE_Quality_Scores) * ( number_of_quality_scores_in_current_position + 10 ) );
+
+		printf ("\n%d %d" , rle_quality_scores_index , number_of_quality_scores_in_current_position);
+		fflush ( stdout);
+
+		rle_quality_scores[rle_quality_scores_index] = ( struct RLE_Quality_Scores* ) malloc (sizeof(struct RLE_Quality_Scores) * ( number_of_quality_scores_in_current_position + 10 ));
 
 		number_of_quality_scores_in_current_position_index = 0;
 		for ( i = 0 ; line[i] != '\0' ; i++ )
@@ -124,7 +124,7 @@ void convertRLEtoQualValues ( char *input_qualityscore_filename, char *output_qu
 	}
 
 	max_read_length = rle_quality_scores_index;
-	quality_score_of_read = ( char* ) malloc ( sizeof(char) * max_read_length );
+	quality_score_of_read = ( char* ) malloc (sizeof(char) * max_read_length);
 
 	/*
 	 * Start constructing the quality scores
@@ -143,16 +143,16 @@ void convertRLEtoQualValues ( char *input_qualityscore_filename, char *output_qu
 		quality_score_of_read[i] = '\0';
 		//printf ( "\n%s" , quality_score_of_read );
 		//fflush ( stdout );
-		fprintf ( fhw , "%s" , quality_score_of_read );
-		fprintf ( fhw , "%s" , "\n" );
+		fprintf (fhw , "%s" , quality_score_of_read);
+		fprintf (fhw , "%s" , "\n");
 		if ( checker_flag == 0 ) break;
 	}
 
-	fclose ( fhr );
-	fclose ( fhw );
+	fclose (fhr);
+	fclose (fhw);
 }
 
-int main ( int argc, char *argv[] )
+int main (int argc, char *argv[])
 {
 	/********************************************************************
 	 * Variable declaration
@@ -166,9 +166,9 @@ int main ( int argc, char *argv[] )
 	/********************************************************************
 	 * Variable initialization
 	 ********************************************************************/
-	strcpy( input_qualityscore_filename , argv[1] );
-	strcpy( output_quality_score_filename , argv[2] );
+	strcpy(input_qualityscore_filename , argv[1]);
+	strcpy(output_quality_score_filename , argv[2]);
 	/********************************************************************/
 
-	convertRLEtoQualValues ( input_qualityscore_filename , output_quality_score_filename );
+	convertRLEtoQualValues (input_qualityscore_filename , output_quality_score_filename);
 }
