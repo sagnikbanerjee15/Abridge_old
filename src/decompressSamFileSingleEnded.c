8b0de85f34ac705260636b8ba3a5fd386caa6222
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-07-04 05:48:02 -0500

    Update decompressSamFileSingleEnded.c

--- src/decompressSamFileSingleEnded.c
+++ src/decompressSamFileSingleEnded.c
@@ -240,7 +240,7 @@ void decompressFile (char *name_of_file_with_quality_scores, char *abridge_index
 		convertToAlignmentSingleEnded (sam_alignment_instance , whole_genome , split_on_tab , split_on_dash , split_on_comma , default_quality_value , flag_save_scores , flag_ignore_mismatches , flag_ignore_soft_clippings , flag_ignore_unmapped_sequences , flag_ignore_quality_score , flag_ignore_sequence_information , &read_number , &total_mapped_reads , read_prefix , fhw , fhr_qual , flag_save_all_quality_scores , number_of_columns , curr_position , current_chromosome , read_names , read_names_stored);
 	}
 
-	printf ("\nTotal mapped reads: %d" , *total_mapped_reads);
+	printf ("\nTotal mapped reads: %d" , total_mapped_reads);
 	fflush (stdout);
 	/*
 	 * Write all unmapped reads to samfile
