c827d9a7878380806ff5d362f73029832bfc0bd6
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-07-03 19:23:48 -0500

    Update deCompressQualityScoresFile.c

--- src/deCompressQualityScoresFile.c
+++ src/deCompressQualityScoresFile.c
@@ -161,7 +161,7 @@ void convertRLEtoQualValues (char *input_qualityscore_filename, char *output_qua
 				fflush (stdout);
 			}
 			checker_flag += rle_quality_scores[i][quality_score_position_index[i]].frequency;
-			printf ("\n Frequency being added %d" , rle_quality_scores[i][quality_score_position_index[i]].frequency);
+			printf ("\n Frequency being added %d i=%d j=%d" , rle_quality_scores[i][quality_score_position_index[i]].frequency , i , quality_score_position_index[i]);
 		}
 		printf ("\nChecker flag %d" , checker_flag);
 		quality_score_of_read[i] = '\0';
