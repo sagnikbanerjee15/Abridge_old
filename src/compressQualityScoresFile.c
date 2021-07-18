7b15dcf2d263726a379535b3cfb85e6b4e640fbe
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-07-18 13:32:07 -0500

    Update compressQualityScoresFile.c

--- src/compressQualityScoresFile.c
+++ src/compressQualityScoresFile.c
@@ -147,9 +147,10 @@ void performColumnWiseRLE (
 	do
 	{
 		line_number++;
-		printf ("\nProcessing line number %d %s" , line_number , line);
+		printf ("\nProcessing line number %d" , line_number);
 		splitByDelimiter (line , '\t' , split_on_tab);
 		expandMDString (split_on_tab[1] , change_indicator);
+		continue;
 		if ( strcmp (split_on_tab[2] , "2") == 0 ) // Reverse the change indicator
 		{
 			i = 0;
@@ -161,7 +162,6 @@ void performColumnWiseRLE (
 				change_indicator[j-- ] = temp;
 			}
 		}
-		continue;
 		//printf ( "\n%s\t%s\n%s" , split_on_tab[0] , split_on_tab[1] , change_indicator );
 		//fflush ( stdout );
 		for ( i = 0 ; split_on_tab[0][i] != '\0' ; i++ )
