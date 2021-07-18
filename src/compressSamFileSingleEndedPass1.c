543443d6a97f0c646e37c72c4feeaac208595f3b
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-07-17 20:20:09 -0500

    Update compressSamFileSingleEndedPass1.c

--- src/compressSamFileSingleEndedPass1.c
+++ src/compressSamFileSingleEndedPass1.c
@@ -150,13 +150,15 @@ void writeToFile (
 					}
 					if ( compressed_ds_pool[i]->icigar[1] != '\0' )
 					{
-						for ( k = 0 ; compressed_ds_pool[i]->icigar[k] != '~' ;
+						for ( k = 0 ;
+								compressed_ds_pool[i]->icigar[k + 1] != '~' ;
 								k++ )
 							fputc (compressed_ds_pool[i]->icigar[k] , fhw_qual);
 					}
 					else
 					{
-						for ( k = 0 ; compressed_ds_pool[i]->icigar[k] != '~' ;
+						for ( k = 0 ;
+								compressed_ds_pool[i]->icigar[k + 1] != '~' ;
 								k++ )
 							fputc (compressed_ds_pool[i - 1]->icigar[k] , fhw_qual);
 					}
