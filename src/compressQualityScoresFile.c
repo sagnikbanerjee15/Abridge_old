39ccb1b5af85fb1b0e31e26ccbdc322b75e9fbbd
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-07-18 14:32:08 -0500

    Update compressQualityScoresFile.c

--- src/compressQualityScoresFile.c
+++ src/compressQualityScoresFile.c
@@ -73,6 +73,8 @@ void expandMDString (char *icigar, char *change_indicator, int val)
 		}
 	}
 	change_indicator[change_indicator_index++ ] = '\0';
+	if ( val == 3540189 )
+		printf ("\n6.change_indicator_index = %d %s" , change_indicator_index , icigar);
 }
 
 void performColumnWiseRLE (
