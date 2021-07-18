4fdbd3f7768a23c2b8f1da46b85245783a98dda8
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-07-18 14:57:17 -0500

    Update compressQualityScoresFile.c

--- src/compressQualityScoresFile.c
+++ src/compressQualityScoresFile.c
@@ -169,7 +169,8 @@ void performColumnWiseRLE (
 	do
 	{
 		line_number++;
-		printf ("\nProcessing line number %d" , line_number);
+		if ( line_number % 100 == 0 )
+			printf ("\nProcessing line number %d" , line_number);
 		splitByDelimiter (line , '\t' , split_on_tab);
 		expandMDString (split_on_tab[1] , change_indicator , line_number);
 		continue;
