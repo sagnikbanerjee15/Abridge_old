4049897aca7b109a75bcfe85ce165a802b43f628
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-07-23 09:33:41 -0500

    Update compressSamFileSingleEndedPass1.c

--- src/compressSamFileSingleEndedPass1.c
+++ src/compressSamFileSingleEndedPass1.c
@@ -559,7 +559,6 @@ void readAlignmentsAndCompress (
 	/*
 	 * Read in the reference sequence information
 	 */
-	return;
 	while ( ( line_len = getline ( &line , &len , fhr) ) != -1 )
 	{
 		if ( line[0] == '@' )
@@ -714,6 +713,7 @@ void readAlignmentsAndCompress (
 		//printf("\nMax_read_at_a_position %d chromosome %s position %d compressed_ds_pool_index %d", compressed_ds_pool[compressed_ds_pool_index]->num_reads, curr_alignment->reference_name, curr_alignment->start_position, compressed_ds_pool_index);
 		reInitializeSamAlignmentInstance (curr_alignment);
 	} while ( ( line_len = getline ( &line , &len , fhr) ) != -1 );
+	return;
 	/*
 	 *Write final data to file
 	 */
