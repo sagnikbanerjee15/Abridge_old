7804a59653a90da4df2f018cd973a84793811e83
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-07-23 15:03:45 -0500

    Update compressSamFileSingleEndedPass1.c

--- src/compressSamFileSingleEndedPass1.c
+++ src/compressSamFileSingleEndedPass1.c
@@ -150,12 +150,16 @@ void writeToFile (
 					}
 					if ( compressed_ds_pool[i]->icigar[1] != '\0' )
 					{
-						fprintf (fhw_qual , "%s" , " Entering here ");
-						fprintf (fhw_qual , "%s" , compressed_ds_pool[i]->icigar);
+						printf ("\nEntering here");
+						fflush (stdout);
 						for ( k = 0 ;
 								compressed_ds_pool[i]->icigar[k + 1] != '~' ;
 								k++ )
+						{
+							printf ("\nk=%d" , k);
+							fflush (stdout);
 							fputc (compressed_ds_pool[i]->icigar[k] , fhw_qual);
+						}
 						fprintf (fhw_qual , "%s" , "String writing complete");
 					}
 					else
