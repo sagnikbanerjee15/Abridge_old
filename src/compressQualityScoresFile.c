6c3a09ac593b256db927bb2a765c0865caf96a4d
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-07-26 07:34:01 -0500

    Update compressQualityScoresFile.c

--- src/compressQualityScoresFile.c
+++ src/compressQualityScoresFile.c
@@ -247,7 +247,7 @@ void performColumnWiseRLE (
 		for ( i = 0 ; split_on_tab[0][i] != '\0' ; i++ )
 		{
 			count_max_reads_each_position[i]++;
-			if ( i == 0 && line_number % 1000 == 0 )
+			if ( i == 0 )
 				printf ("\nline=%d %d" , line_number , count_max_reads_each_position[i]);
 			//printf ( "\ni=%d split_on_tab[0][i]=%c qsRLE[i]->score_character=%c " , i , split_on_tab[0][i] , qsRLE[i]->score_character );
 			if ( split_on_tab[0][i] == qsRLE[i]->score_character || ( save_exact_quality_scores == 0 && change_indicator[i] == '1' ) )
