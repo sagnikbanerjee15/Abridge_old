e6622dbb1f143969c46e314d08d265749e3aca0f
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-07-26 07:17:57 -0500

    Update compressQualityScoresFile.c

--- src/compressQualityScoresFile.c
+++ src/compressQualityScoresFile.c
@@ -246,7 +246,10 @@ void performColumnWiseRLE (
 			//if ( i > max_len_sequence ) max_len_sequence = i;
 			//printf ( "\ni=%d split_on_tab[0][i]=%c qsRLE[i]->score_character=%c " , i , split_on_tab[0][i] , qsRLE[i]->score_character );
 			if ( split_on_tab[0][i] == qsRLE[i]->score_character || ( save_exact_quality_scores == 0 && change_indicator[i] == '1' ) )
+			{
 				qsRLE[i]->frequency++;
+				count_max_reads_each_position[i]++;
+			}
 			else
 			{
 				if ( qsRLE[i]->frequency > 1 )
@@ -254,7 +257,7 @@ void performColumnWiseRLE (
 					sprintf(str , "%lld" , qsRLE[i]->frequency);
 					fprintf (fhw_each_position[i] , "%s" , str);
 				}
-				count_max_reads_each_position[i] += qsRLE[i]->frequency;
+				//count_max_reads_each_position[i] += qsRLE[i]->frequency;
 				fputc (qsRLE[i]->score_character + 30 , fhw_each_position[i]);
 				qsRLE[i]->frequency = 1;
 				qsRLE[i]->score_character = split_on_tab[0][i];
