7365b0a22bf120ffd7132a7a20cee07b30b67a74
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-09-06 14:57:47 -0500

    Update compressSamFilePairedEndedPass1.c

--- src/compressSamFilePairedEndedPass1.c
+++ src/compressSamFilePairedEndedPass1.c
@@ -716,12 +716,11 @@ void compressPairedEndedAlignments (
 			compressed_ds_pool[compressed_ds_pool_index]->position = curr_alignment->start_position;
 			strcpy(qual_scores[quality_score_index] , curr_alignment->qual);
 			strcpy(read_names[quality_score_index] , curr_alignment->read_name);
-			continue;
 			quality_score_index++;
 			//printf("\n2. Max_read_at_a_position %d chromosome %s position %d compressed_ds_pool_index %d", compressed_ds_pool[compressed_ds_pool_index]->num_reads, curr_alignment->reference_name, curr_alignment->start_position, compressed_ds_pool_index);
 			compressed_ds_pool_index++;
 			printf ("\nquality_score_index %lld compressed_ds_pool_index %lld" , quality_score_index , compressed_ds_pool_index);
-
+			continue;
 			//printf("\n Writing Reference to file %s %d", reference_info[reference_sequence_index]->line, reference_sequence_index);
 			//fflush(stdout);
 			fprintf (fhw_pass1 , "%s" , reference_info[reference_sequence_index]->line);
