3f72ad4423e2ca43b19a692c0528e97193591a42
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-09-11 09:18:56 -0500

    Update compressSamFileSingleEndedPass1.c

--- src/compressSamFileSingleEndedPass1.c
+++ src/compressSamFileSingleEndedPass1.c
@@ -602,7 +602,7 @@ void readAlignmentsAndCompress (
 		current_position = curr_alignment->start_position;
 		//printSamAlignmentInstance(curr_alignment,0);
 		generateIntegratedCigarSingleEnded (curr_alignment , flag_save_scores , flag_ignore_soft_clippings , flag_ignore_mismatches , flag_ignore_unmapped_sequences , flag_ignore_quality_score , whole_genome , sam_alignment_instance_diagnostics , number_of_records_read , run_diagnostics);
-		printf ("\n Position:%lld iCIGAR: %s" , curr_alignment->start_position , curr_alignment->icigar);
+		//printf ("\n Position:%lld iCIGAR: %s" , curr_alignment->start_position , curr_alignment->icigar);
 		if ( strlen (prev_reference_name) == 0 ) // 1st chromosome - initialize stuffs
 		{
 			//printf("\n1. compressed_ds_pool_index %d", compressed_ds_pool_index);
