4b453b21b07afb6a5be4f936359260d667aa63af
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-09-13 11:21:58 -0500

    Update compressQualityScoresFile.c

--- src/compressQualityScoresFile.c
+++ src/compressQualityScoresFile.c
@@ -23,7 +23,7 @@ void expandMDString (char *icigar, char *change_indicator, int val)
 
 	/********************************************************************/
 
-	printf ("\n%d %s" , val , icigar);
+	if ( val > 700000 ) printf ("\n%d %s" , val , icigar);
 	//extractNHfromicigar (icigar , strlen (icigar));
 	num = 0;
 	for ( i = 0 ; icigar[i] != '\0' ; i++ )
