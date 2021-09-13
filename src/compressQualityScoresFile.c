424711d63287c491866a8882f11d5ac3ebd2790e
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-09-13 07:39:56 -0500

    Update compressQualityScoresFile.c

--- src/compressQualityScoresFile.c
+++ src/compressQualityScoresFile.c
@@ -312,6 +312,7 @@ void performColumnWiseRLE (
 		strcpy(output_filename_for_each_position , output_quality_score_filename);
 		strcat(output_filename_for_each_position , str);
 		printf ("\nAttempting to read from %s" , output_filename_for_each_position);
+		continue;
 		fhr = fopen (output_filename_for_each_position , "r");
 		if ( fhr == NULL )
 		{
