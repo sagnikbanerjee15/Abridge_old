f920a24d62828939f28d2897de5188c3c15cc241
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-09-22 12:33:14 -0500

    Update compressQualityScoresFile.c

--- src/compressQualityScoresFile.c
+++ src/compressQualityScoresFile.c
@@ -238,7 +238,7 @@ void performColumnWiseRLE (
 		splitByDelimiter (line , '\t' , split_on_tab);
 		strcpy(qual_score , split_on_tab[0]);
 		//printf ("\nCIGAR = %s" , split_on_tab[1]);
-
+		continue;
 		expandMDString (split_on_tab[1] , change_indicator , line_number , max_read_length);
 		continue;
 		if ( strcmp (split_on_tab[2] , "2") == 0 ) // Reverse the change indicator
