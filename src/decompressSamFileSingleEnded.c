5204da6ef406b1e4193027ce4dde6fff0d04ec69
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-10-02 15:59:36 -0500

    Update decompressSamFileSingleEnded.c

--- src/decompressSamFileSingleEnded.c
+++ src/decompressSamFileSingleEnded.c
@@ -231,6 +231,7 @@ void decompressFile (
 			//fflush (stdout);
 
 		}
+		printf ("\nCheckpoint 2 line_num = %d" , line_num);
 		if ( max_number_of_commas > ROWS_split_on_comma )
 		{
 			//printf ("\nB--> max_number_of_commas %d ROWS_split_on_comma %d" , max_number_of_commas , ROWS_split_on_comma);
@@ -258,8 +259,9 @@ void decompressFile (
 				curr_position++;
 			else curr_position += strtol (split_on_tab[0] , &convert_to_int_temp , 10);
 		}
+		printf ("\nCheckpoint 3 line_num = %d" , line_num);
 		convertToAlignmentSingleEnded (sam_alignment_instance , whole_genome , split_on_tab , split_on_dash , split_on_comma , split_on_tilde , default_quality_value , flag_ignore_scores , flag_ignore_mismatches , flag_ignore_soft_clippings , flag_ignore_unmapped_sequences , flag_ignore_quality_score , flag_ignore_sequence_information , &read_number , &total_mapped_reads , read_prefix , fhw , fhr_qual , flag_save_all_quality_scores , number_of_columns , curr_position , current_chromosome , read_names , read_names_stored);
-		printf ("\nline_num = %d End" , line_num);
+		printf ("\nCheckpoint 4 line_num = %d" , line_num);
 		fflush (stdout);
 	}
 
