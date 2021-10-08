c0f77093c39450791a6f59d741ecea4cc43108fb
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-10-08 09:31:38 -0500

    Update buildAbridgeIndex.c

--- src/buildAbridgeIndex.c
+++ src/buildAbridgeIndex.c
@@ -416,7 +416,7 @@ void findContinousClusters (
 		}
 		line_len_previous = line_len;
 		//if (strcmp(current_reference_sequence, "MT") == 0) printf("\nLine read from file:%s", line);
-		//fflush (fhw);
+		fflush (fhw);
 	}
 	//byte_number_end_cluster = ftell ( fhr_pass1 ) - line_len - line_len_previous;
 	byte_number_end_cluster = ftell (fhr_pass1);
