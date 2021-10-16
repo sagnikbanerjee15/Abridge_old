0ae47a9abbd3e2518d5bd0856ddca5765f32b834
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-10-16 10:11:43 -0500

    Update function_definitions.h

--- src/function_definitions.h
+++ src/function_definitions.h
@@ -3519,6 +3519,7 @@ void convertToAlignmentPairedEnded (
 	}
 	else if ( number_of_columns == 3 )
 	{
+		return;
 		int max_number_of_commas = 0, number_of_commas = 0;
 		for ( i = 0 ; split_on_tab[1][i] != '\0' ; i++ )
 			if ( split_on_tab[1][i] == ',' ) number_of_commas++;
