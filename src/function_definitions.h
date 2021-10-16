ea7b4ee2d2b2d97c0645ba8e411fd3921c193892
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-10-16 09:45:19 -0500

    Update function_definitions.h

--- src/function_definitions.h
+++ src/function_definitions.h
@@ -3569,7 +3569,8 @@ void convertToAlignmentPairedEnded (
 			strcpy (sam_alignment_instance->icigar , split_on_tilde[0]);
 			//printf ("\nj=%d number_of_distinct_cigars_in_a_line=%d Inside ICIGAR %s" , j , number_of_distinct_cigars_in_a_line , sam_alignment_instance->icigar);
 			//fflush (stdout);
-			convertIcigarToCigarandMDPairedEnded (whole_genome , sam_alignment_instance , chromosome , flag_ignore_mismatches , flag_ignore_soft_clippings , flag_ignore_unmapped_sequences , flag_ignore_quality_score , flag_ignore_sequence_information , default_quality_value , samflag_dictionary , number_of_unique_samformatflags , samformatflag_replacer_characters);
+
+			//convertIcigarToCigarandMDPairedEnded (whole_genome , sam_alignment_instance , chromosome , flag_ignore_mismatches , flag_ignore_soft_clippings , flag_ignore_unmapped_sequences , flag_ignore_quality_score , flag_ignore_sequence_information , default_quality_value , samflag_dictionary , number_of_unique_samformatflags , samformatflag_replacer_characters);
 
 			/*if ( sam_alignment_instance->start_position == 27381 && strcmp (sam_alignment_instance->reference_name , "1") == 0 )
 			 {
