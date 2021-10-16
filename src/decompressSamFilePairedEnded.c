150578ca4fb54c0acf7b43b30be4d9d59e5b6d2c
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-10-16 10:03:10 -0500

    push

--- src/decompressSamFilePairedEnded.c
+++ src/decompressSamFilePairedEnded.c
@@ -249,6 +249,7 @@ void decompressFile (
 			curr_position++;
 		else if ( number_of_columns == 3 )
 			curr_position += strtol (split_on_tab[0] , &convert_to_int_temp , 10);
+		if ( strcmp ("Pt" , current_chromosome) == 0 ) printf ("%s" , buffer);
 		convertToAlignmentPairedEnded (sam_alignment_instance , whole_genome , split_on_tab , split_on_dash , split_on_comma , split_on_tilde , read_names , default_quality_value , flag_ignore_scores , flag_ignore_mismatches , flag_ignore_soft_clippings , flag_ignore_unmapped_sequences , flag_ignore_quality_score , flag_ignore_sequence_information , &read_number , &total_mapped_reads , fhw , fhr_qual , flag_save_all_quality_scores , number_of_columns , curr_position , current_chromosome , samflag_dictionary , number_of_unique_samformatflags , samformatflag_replacer_characters);
 	}
 
