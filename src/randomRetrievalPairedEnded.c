05efc0b56fe010cf4ac6858591afa2bee47b0174
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-10-28 05:45:05 -0500

    Update randomRetrievalPairedEnded.c

--- src/randomRetrievalPairedEnded.c
+++ src/randomRetrievalPairedEnded.c
@@ -186,11 +186,11 @@ int main (int argc, char *argv[])
 	max_reads = 0;
 	for ( i = abridge_match_start_index ; i <= abridge_match_end_index ; i++ )
 	{
-		/*
-		 printf ("\n%s %lld %lld %lld %lld %lld %lld" , abridge_index->chromosome[i] , abridge_index->start[i] , abridge_index->end[i] , abridge_index->start_byte[i] , abridge_index->end_byte[i] , abridge_index->start_byte_qual[i] , abridge_index->end_byte_qual[i]);
-		 fflush (stdout);
-		 continue;
-		 */
+
+		printf ("\n%s %lld %lld %lld %lld %lld %lld" , abridge_index->chromosome[i] , abridge_index->start[i] , abridge_index->end[i] , abridge_index->start_byte[i] , abridge_index->end_byte[i] , abridge_index->start_byte_qual[i] , abridge_index->end_byte_qual[i]);
+		fflush (stdout);
+		//continue;
+
 		fseek_ret_val = fseek (fhr_pass1 , abridge_index->start_byte[i] , SEEK_SET);
 		buffer_for_pass1[0] = '\0';
 		fread_ret_val = fread (buffer_for_pass1 , 1 , abridge_index->end_byte[i] - abridge_index->start_byte[i] , fhr_pass1);
