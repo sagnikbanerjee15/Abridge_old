b28a1296bcf7294771cac469d3ba80fab5f73400
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-10-28 16:47:07 -0500

    Update generateCoverage.c

--- src/generateCoverage.c
+++ src/generateCoverage.c
@@ -173,7 +173,7 @@ void generateCoverageFromCompressedMappedFile (
 
 			printf ("\n%s number_of_bytes_read_from_compressed_file = %d max_bytes_for_current_index_entry = %d" , abridge_index->chromosome[i] , number_of_bytes_read_from_compressed_file , max_bytes_for_current_index_entry);
 			fflush (stdout);
-
+			continue;
 			line_len = getline ( &buffer_for_pass1 , &len , fhr_pass1);
 			if ( buffer_for_pass1[0] == '@' )
 			{
