2be54726f084c2c53c835541a36eb1dacc16a454
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-10-28 07:24:14 -0500

    Update randomRetrievalSingleEnded.c

--- src/randomRetrievalSingleEnded.c
+++ src/randomRetrievalSingleEnded.c
@@ -167,7 +167,7 @@ int main (int argc, char *argv[])
 	 */
 	for ( i = abridge_match_start_index ; i <= abridge_match_end_index ; i++ )
 	{
-		loop_num = 0;
+		loop_num++;
 		if ( loop_num == 2 ) break;
 		/*
 		 printf ("\n%s %lld %lld %lld %lld %lld %lld" , abridge_index->chromosome[i] , abridge_index->start[i] , abridge_index->end[i] , abridge_index->start_byte[i] , abridge_index->end_byte[i] , abridge_index->start_byte_qual[i] , abridge_index->end_byte_qual[i]);
