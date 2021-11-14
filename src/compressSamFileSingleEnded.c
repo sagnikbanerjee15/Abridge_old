998ad9bf0be31754a066d758982ce61290dbdcff
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-11-14 10:38:33 -0600

    Update compressSamFileSingleEnded.c

--- src/compressSamFileSingleEnded.c
+++ src/compressSamFileSingleEnded.c
@@ -63,7 +63,8 @@ void writeToFile (
 		struct Cigar_Items *cigar_items_instance)
 {
 	printf ("\nInside writeToFile\n");
-	//fflush (stdout);
+	fflush (stdout);
+	return;
 	int i, j, k, l, m;
 	char str[1000];
 	char qual[MAX_SEQ_LEN];
