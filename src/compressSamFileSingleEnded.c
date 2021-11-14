7f223a1a6673a20e41bd192bed9c975bb480454a
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-11-14 11:09:46 -0600

    Update compressSamFileSingleEnded.c

--- src/compressSamFileSingleEnded.c
+++ src/compressSamFileSingleEnded.c
@@ -682,7 +682,7 @@ void readAlignmentsAndCompress (
 		 fflush(stdout);
 		 }*/
 
-		printf ("\n%s" , line);
+		//printf ("\n%s" , line);
 		number_of_fields = splitByDelimiter (line , '\t' , split_line);
 		populateSamAlignmentInstance (curr_alignment , split_line , number_of_fields , split_tags);
 		strcpy(curr_reference_name , curr_alignment->reference_name);
