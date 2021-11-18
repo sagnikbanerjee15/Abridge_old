c1ff5383ccf28bcd75214982641b7f4e475e20ed
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-11-18 12:14:24 -0600

    Update compressSamFilePairedEnded.c

--- src/compressSamFilePairedEnded.c
+++ src/compressSamFilePairedEnded.c
@@ -809,6 +809,7 @@ void compressPairedEndedAlignments (
 				//printf ("\n4. compressed_ds_pool_index %d" , compressed_ds_pool_index);
 				//fflush(stdout);
 				reModeliCIGARSPairedEnded (compressed_ds_pool , compressed_ds_pool_rearranged , already_processed , compressed_ds_pool_index , modified_icigars , samformatflag_replacer_characters);
+				continue;
 				writeToFile (flag_save_all_quality_scores , flag_save_exact_quality_scores , fhw_qual , fhw_pass1 , compressed_ds_pool_rearranged , compressed_ds_pool_index , write_to_file_col1 , write_to_file_col2 , write_to_file_col3 , encoded_string , &curr_commas , qual_scores , quality_score_index , samformatflag_replacer_characters , number_of_unique_samformatflags , samflag_dictionary , flag_ignore_soft_clippings , cigar_items_instance , line_to_be_written_to_file , list_of_read_names , list_of_qual_scores , qual_for_writeToFile);
 				if ( max_commas < curr_commas ) max_commas = curr_commas;
 				//printf ( "\n%lld %lld" , curr_commas , max_commas );
