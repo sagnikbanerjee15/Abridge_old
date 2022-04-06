9f6a468f62cdb224d2fb1b509569000b658eea2d
Sagnik Banerjee <Sagnik.Banerjee@bms.com>
2022-04-06 14:49:51 -0500

    Update decompressSamFileSingleEnded.c

--- src/decompressSamFileSingleEnded.c
+++ src/decompressSamFileSingleEnded.c
@@ -238,7 +238,6 @@ void decompressFile(
 		else
 			read_names_stored = 0;
 
-		continue;
 		//printf ("\nRead present? %d %s" , strstr (buffer , "abridge_") , buffer);
 		if (buffer[0] == '@')
 		{
@@ -260,6 +259,7 @@ void decompressFile(
 			//fflush (stdout);
 			continue;
 		}
+		continue;
 		number_of_commas_in_each_line = 0;
 		for (i = 0; buffer[i] != '\0'; i++)
 			if (buffer[i] == ',')
