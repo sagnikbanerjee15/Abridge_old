1fecb5bc2f49dceee453a64c1e2d989f790ee0a4
Sagnik Banerjee <Sagnik.Banerjee@bms.com>
2022-04-07 12:31:41 -0500

    Update decompressSamFileSingleEnded.c

--- src/decompressSamFileSingleEnded.c
+++ src/decompressSamFileSingleEnded.c
@@ -322,8 +322,7 @@ void decompressFile(
 						10);
 		}
 		//printf("\nCheckpoint 3 line_num = %d", line_num);
-		if (strstr(buffer, ",3Zg,") == NULL && strstr(buffer, "3Zg,") == NULL
-				&& strstr(buffer, ",3Zg") == NULL)
+		if (strstr(buffer, "3Zg,2hWh,dpt,DCZz") == NULL)
 			continue;
 		printf("\nThe compressed data %s", buffer);
 		convertToAlignmentSingleEnded(
