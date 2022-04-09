0c70ff12e014374ceaa8c4d284db4536a958e100
Sagnik Banerjee <Sagnik.Banerjee@bms.com>
2022-04-08 22:22:11 -0500

    Update decompressSamFileSingleEnded.c

--- src/decompressSamFileSingleEnded.c
+++ src/decompressSamFileSingleEnded.c
@@ -144,7 +144,7 @@ void decompressFile(
 				max_commas = num_commas;
 		}
 	}
-	exit(0);
+
 	COLS_split_on_tab = max_line_len;
 	ROWS_split_on_comma = max_commas;
 	fclose(fhr);
@@ -212,7 +212,7 @@ void decompressFile(
 	/********************************************************************/
 
 	writeSequenceHeaders(fhw, genome_filename, 1);
-
+	exit(0);
 	line_num = 0;
 	line_len = getline(&buffer, &len, fhr);
 	/*
