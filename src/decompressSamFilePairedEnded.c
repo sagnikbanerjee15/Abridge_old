03da1ef029471a0a4812f1aaca81754735d58f39
sagnikbanerjee15 <sagnik.banerjee@bms.com>
2022-11-24 12:08:21 -0800

    push

--- src/decompressSamFilePairedEnded.c
+++ src/decompressSamFilePairedEnded.c
@@ -98,7 +98,8 @@ static error_t parse_opt( int key, char *arg, struct argp_state *state )
 					|| strcmp( arguments->compressedfile, "" ) == 0
 					|| strcmp( arguments->unmappedreadsfilename, "" ) == 0
 					|| strcmp( arguments->qualityscoresfilename, "" ) == 0
-					|| strcmp( arguments->dictionaryfilename, "" ) == 0 )
+					|| strcmp( arguments->dictionaryfilename, "" ) == 0
+					|| arguments->maxreadsineachline == 0 )
 			{
 				argp_usage( state );
 			}
