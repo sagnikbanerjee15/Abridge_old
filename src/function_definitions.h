13af9ead8f2be93853e724655ab2020045716238
sagnikbanerjee15 <sagnik.banerjee@bms.com>
2022-11-25 15:54:13 -0800

    push

--- src/function_definitions.h
+++ src/function_definitions.h
@@ -3548,6 +3548,7 @@ void generateReadSequenceAndMDString(
 		}
 	}
 
+	print( "\nLength of MD: %d", strlen( sam_alignment_instance->MD ) );
 	for ( i = 0; sam_alignment_instance->MD[i] != '\0'; i++ )
 		if ( sam_alignment_instance->MD[i] >= 'A'
 				&& sam_alignment_instance->MD[i] <= 'Z'
